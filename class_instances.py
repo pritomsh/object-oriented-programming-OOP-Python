395ae1198e50b95823b109b2729d566392f1812d
Pritom Saha <pritom1807@gmail.com>
2023-10-20 10:59:12 +0600

    start learning class in oop

---
+++ class_instances.py
@@ -0,0 +1,10 @@
+class Employee:
+    def __init__(self, firstN, lastN, pay) -> None:
+        pass
+
+
+employe1 = Employee("Pritom","Saha",100)
+employe2 = Employee("Himel","Saha",100)
+
+print(employe1)
+print(employe2)
